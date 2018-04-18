70cbd445b201c25f48151fab64325cd5dc1faae2
Mario Serrano <maseflores79@gmail.com>
2018-04-18 16:38:30 -0700

    Agregado servicio de modificacion de usuario

--- routes/usuario.js
+++ routes/usuario.js
@@ -63,4 +63,53 @@ app.post('/', (req, res) => {
     });
 });
 
+// Actualizar un usuario 
+app.put('/:id', (req, res) => {
+    var id = req.params.id;
+    var body = req.body;
+
+    Usuario.findById(id, (err, usuario) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario.',
+                errors: err
+            });
+        }
+
+        if (!usuario) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'No se encuentra usuario con el id ' + id + '.',
+                errors: { message: 'No existe usuario con ese ID' }
+            });
+        }
+
+        usuario.nombre = body.nombre;
+        usuario.email = body.email;
+        usuario.role = body.role;
+
+        usuario.save((err, usuarioGuardado) => {
+
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar usuario con el id ' + id + '.',
+                    errors: err
+                });
+            }
+
+            usuarioGuardado.password = ':)';
+
+            res.status(200).json({
+                ok: true,
+                usuario: usuarioGuardado
+            });
+
+        });
+
+    });
+});
+
 module.exports = app;
