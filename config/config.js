b93046d70096a99f4ec8c2c47db7a1893051a93c
Mario Serrano <maseflores79@gmail.com>
2018-04-30 20:17:56 -0700

    primeras validaciones de autenticacion con google signin en el backend

--- config/config.js
+++ config/config.js
@@ -1,1 +1,3 @@
 module.exports.SEED = '@este-es@-un-seed-dificil';
+//google
+module.exports.CLIENT_ID = '501471606436-8a4bkqck9pt659bltrijac6t8sc1nqf0.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,60 @@
-/*jshint esversion: 6 */
 var express = require('express');
 var bcrypt = require('bcryptjs');
 
 /* https://github.com/auth0/node-jsonwebtoken */
 var jwt = require('jsonwebtoken');
-
 var semilla = require('../config/config').SEED;
+
+// google
+const CLIENT_ID = require('../config/config').CLIENT_ID;
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
 
 var app = express();
 var Usuario = require('../models/usuario');
 
+
+/* Autenticación de google */
+async function verify(token) {
+    //el await es una promesa
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID // Specify the CLIENT_ID of the app that accesses the backend
+            // Or, if multiple clients access the backend:
+            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+}
+
+app.post('/google', async(req, res) => {
+
+    var token = req.body.token;
+
+    var googleUser = await verify(token)
+        .catch(e => {
+            res.status(403).json({
+                ok: false,
+                mensaje: 'Token no válido'
+            });
+        });
+
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Ok!',
+        googleUser: googleUser
+    });
+});
+
+/* Autenticación normal */
 app.post('/', (req, res) => {
 
     var body = req.body;
