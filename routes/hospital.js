4c06238f3658481cea238206874fcf02e0f8c3a6
Mario Serrano <maseflores79@gmail.com>
2018-05-14 17:47:48 -0700

    Creacion de nuevo metodo en servicio de hospital

--- routes/hospital.js
+++ routes/hospital.js
@@ -5,6 +5,37 @@ var app = express();
 var Hospital = require('../models/hospital');
 var mdAutenticacion = require('../middlewares/autenticacion');
 
+// =====================================
+// 	 .
+// =====================================
+app.get('/:id', (req, res) => {
+    var id = req.params.id;
+    Hospital.findById(id)
+        .populate('usuario', 'nombre img email')
+        .exec((err, hospital) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error al buscar hospital',
+                    errors: err
+                });
+            }
+            if (!hospital) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'El hospital con el id ' + id + ' no existe ',
+                    errors: {
+                        message: 'No existe un hospital con ese ID '
+                    }
+                });
+            }
+            res.status(200).json({
+                ok: true,
+                hospital: hospital
+            });
+        });
+});
+
 app.get('/', (req, res, next) => {
 
     var desde = req.query.desde || 0;
